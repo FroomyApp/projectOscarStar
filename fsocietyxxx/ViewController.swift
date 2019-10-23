a606206901c4457ccd0b25581172c1e726154e86
Tyler Phillips <thetylerjp@icloud.com>
2019-10-23 17:23:24 -0400

    Updated Xcode project

--- fsocietyxxx/ViewController.swift
+++ fsocietyxxx/ViewController.swift
@@ -14,9 +14,32 @@ class ViewController: UIViewController {
         let label = UILabel()
         label.text = "Welcome to fsocietyxxx"
         label.textColor = .green
-        label.font = UIFont(name: "Menlo", size: 28)
+        label.font = UIFont(name: "Menlo", size: 25)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
+    }()
+        
+    let emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "fsociety email"
+        textField.backgroundColor = .darkGray
+        textField.textColor = .green
+        textField.font = UIFont(name: "Menlo", size: 18)
+        textField.layer.cornerRadius = 5
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "fsociety password"
+        textField.backgroundColor = .darkGray
+        textField.textColor = .green
+        textField.font = UIFont(name: "Menlo", size: 18)
+        textField.isSecureTextEntry = true
+        textField.layer.cornerRadius = 5
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
     }()
     
     let continueButton: UIButton = {
@@ -38,12 +61,29 @@ class ViewController: UIViewController {
     
     func setupObkectConstraints() {
         view.addSubview(fsocietyxxxLabel)
-        fsocietyxxxLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         fsocietyxxxLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        fsocietyxxxLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 185).isActive = true
+        fsocietyxxxLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16).isActive = true
+        fsocietyxxxLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16).isActive = true
+        fsocietyxxxLabel.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        
+        view.addSubview(emailTextField)
+        emailTextField.topAnchor.constraint(equalTo: fsocietyxxxLabel.bottomAnchor, constant: 15).isActive = true
+        emailTextField.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        emailTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16).isActive = true
+        emailTextField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16).isActive = true
+        
+        view.addSubview(passwordTextField)
+        passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 10).isActive = true
+        passwordTextField.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        passwordTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16).isActive = true
+        passwordTextField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16).isActive = true
         
         view.addSubview(continueButton)
-        continueButton.topAnchor.constraint(equalTo: fsocietyxxxLabel.bottomAnchor, constant: 10).isActive = true
+        continueButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 10).isActive = true
         continueButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        continueButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
+            
     }
 
 }
