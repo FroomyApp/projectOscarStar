83060c21bf81b055ddeb47caf5a2a18a250f8f6f
Tyler Phillips <thetylerjp@icloud.com>
2019-10-24 09:37:41 -0400

    iOS App Readded.

---
+++ fsocietyxxx/fsocietyxxx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fsocietyxxx
+//
+//  Created by Tyler P Admin on 10/24/19.
+//  Copyright © 2019 Froomy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
